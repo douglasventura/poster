049940a610f0e3040b3b6d62490b481431ad05cd
Douglas Ventura <douglasventura.pe@gmail.com>
2023-01-11 10:11:17 -0300

    Database instantiation

---
+++ src/main/java/com/douglasventura/workshopmongo/Instantiation.java
@@ -0,0 +1,31 @@
+package com.douglasventura.workshopmongo;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.douglasventura.workshopmongo.domain.User;
+import com.douglasventura.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    userRepository.deleteAll();
+
+    User maria = new User(null, "Maria Brown", "maria@gmail.com");
+    User alex = new User(null, "Alex Green", "alex@gmail.com");
+    User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+    userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+  }
+
+}
