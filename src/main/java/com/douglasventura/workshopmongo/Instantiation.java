7376f294e0c23d1c5d51ccd97006aa8033f22b69
Douglas Ventura <douglasventura.pe@gmail.com>
2023-01-13 13:59:36 -0300

    Post entity implemented with nested user

--- src/main/java/com/douglasventura/workshopmongo/Instantiation.java
+++ src/main/java/com/douglasventura/workshopmongo/Instantiation.java
@@ -1,12 +1,18 @@
 package com.douglasventura.workshopmongo;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.douglasventura.workshopmongo.domain.Post;
 import com.douglasventura.workshopmongo.domain.User;
+import com.douglasventura.workshopmongo.repository.PostRepository;
 import com.douglasventura.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,6 +20,9 @@ public class Instantiation implements CommandLineRunner {
 
   @Autowired
   UserRepository userRepository;
+
+  @Autowired
+  PostRepository postRepository;
 
   @Override
   public void run(String... args) throws Exception {
@@ -25,6 +34,13 @@ public class Instantiation implements CommandLineRunner {
     User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
     userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+    postRepository.deleteAll();
+
+    Post post1 = new Post(null, Instant.parse("2018-03-21T00:00:00Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+    Post post2 = new Post(null, Instant.parse("2018-03-23T00:00:00Z"), "Bom dia", "Acordei feliz hoje!", alex);
+
+    postRepository.saveAll(Arrays.asList(post1, post2));
 
   }
 
