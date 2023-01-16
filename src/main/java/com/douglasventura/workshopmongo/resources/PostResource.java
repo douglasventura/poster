4e96d87e78f06a6f163796d02c2a2545480ff317
Douglas Ventura <douglasventura.pe@gmail.com>
2023-01-16 12:10:43 -0300

    Simple query using query methods

--- src/main/java/com/douglasventura/workshopmongo/resources/PostResource.java
+++ src/main/java/com/douglasventura/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.douglasventura.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglasventura.workshopmongo.domain.Post;
@@ -23,4 +26,9 @@ public class PostResource {
     return ResponseEntity.ok().body(post);
   }
 
+  @GetMapping(value = "/titlesearch")
+  public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+    List<Post> posts = postService.findByTitle(text);
+    return ResponseEntity.ok().body(posts);
+  }
 }
---
+++ src/main/java/com/douglasventura/workshopmongo/resources/util/URL.java
@@ -0,0 +1,15 @@
+package com.douglasventura.workshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+
+  public static String decoderParams(String text) {
+    try {
+      return URLDecoder.decode(text, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      return "";
+    }
+  }
+}
